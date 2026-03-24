677e22fbbdcf881c86d0b4195f101d9f9d60a466
MayWuRu <151303255+MayWuRu@users.noreply.github.com>
2026-03-24 16:56:54 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/**
- * WuRu Shipping - Configuration File
- * สำหรับเก็บค่าลายนิ้วมือดิจิทัล (Hash) ของรหัสผ่านที่ถูกต้อง
- */
-window.SECRETS = {
-    // ค่า SHA-256 Hash ที่ถูกต้องของรหัสผ่าน "6360"
-    ADMIN_HASH: '8ad38db8cfc0cf1f9d8190d2c0301b97ddc148359388daf00c0ba4bb058cee91'
-};
